1ee6801e221e4e0f8c90f9ba2d808885c4fc6949
PineAG <pein.agail@gmail.com>
2023-01-09 14:22:45 +0800

    bugfix

--- projects/dualies/src/ui/routes/PanelExhibition.tsx
+++ projects/dualies/src/ui/routes/PanelExhibition.tsx
@@ -2,13 +2,20 @@ import { PreviewWidget } from "../components/widgets"
 import { PanelIdProvider, usePanel, useWidgetListOfPanel, WidgetProvider } from "../backend"
 import { unwrapAsyncSubs } from "../components/subs"
 import { usePanelIdFromParams } from "./utils"
+import { PanelSize, PanelSizeContext } from "../components/context"
 
 function PanelExhibitionBody() {
     const panelReq = usePanel()
     const widgetsReq = useWidgetListOfPanel()
-    return unwrapAsyncSubs(panelReq, panel => (
-        unwrapAsyncSubs(widgetsReq, widgets => (
+    return unwrapAsyncSubs(panelReq, panel => {
+        const panelSize: PanelSize = {
+            scale: 1,
+            configSize: panel.size,
+            actualRect: {x: 0, y: 0, ...panel.size}
+        }
+        return unwrapAsyncSubs(widgetsReq, widgets => (
         <div style={panel.size}>
+            <PanelSizeContext.Provider value={panelSize}>
             {widgets.map(widget => (
                 <WidgetProvider value={widget} key={widget.id}>
                     <PreviewWidget
@@ -16,9 +23,10 @@ function PanelExhibitionBody() {
                     />
                 </WidgetProvider>
             ))}
+            </PanelSizeContext.Provider>
         </div>
         ))
-    ))
+    })
 }
 
 export function PanelExhibitionPage(){
